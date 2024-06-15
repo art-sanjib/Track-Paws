d26423ffe2c7dcb719fb476def1ac6b5a6d9741b
developerpritam <pritambag426@gmail.com>
2024-06-15 23:12:33 +0530

    Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+       },
+    //    backgroundImage: {
+    //     'hero-pattern': "url('images/cat-location.png')",
+    //  },
+    },
+  },
+  plugins: [],
+}
+
